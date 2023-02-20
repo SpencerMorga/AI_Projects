5ce56a799f3c22f0ef0c7870336d5982eb7beb1b
spencer.morga <spencer.morga@icloud.com>
2023-02-19 16:05:58 -0800

    started genetic learning

---
+++ NeuralNetwork/GeneticLearning.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    internal class GeneticLearning
+    {
+        NeuralNetwork net;
+        Random random;
+        double mutationRate;
+        public GeneticLearning(NeuralNetwork net, double mutationRate, Random random=null)
+        {
+            this.net = net;
+            this.random = random == null ? new Random() : random;
+            this.mutationRate = mutationRate;
+        }
+
+        public void 
+    }
+}
